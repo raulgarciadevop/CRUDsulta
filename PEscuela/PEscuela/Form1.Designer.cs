9f42dad71db27068aa6c2c55fd0f952a29d53381
Raul Garcia <joragarciace@ittepic.edu.mx>
2020-03-25 22:18:31 -0700

    init commit

---
+++ PEscuela/PEscuela/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace PEscuela
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.gridAlumnos = new System.Windows.Forms.DataGridView();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApeP = new System.Windows.Forms.TextBox();
+            this.txtAPM = new System.Windows.Forms.TextBox();
+            this.numEdad = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridAlumnos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numEdad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtBuscar);
+            this.tabPage1.Controls.Add(this.gridAlumnos);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Listado";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnCancelar);
+            this.tabPage2.Controls.Add(this.btnGuardar);
+            this.tabPage2.Controls.Add(this.btnEditar);
+            this.tabPage2.Controls.Add(this.btnNuevo);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.numEdad);
+            this.tabPage2.Controls.Add(this.txtAPM);
+            this.tabPage2.Controls.Add(this.txtApeP);
+            this.tabPage2.Controls.Add(this.txtNombre);
+            this.tabPage2.Controls.Add(this.txtID);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Registro";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // gridAlumnos
+            // 
+            this.gridAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridAlumnos.Location = new System.Drawing.Point(8, 86);
+            this.gridAlumnos.Name = "gridAlumnos";
+            this.gridAlumnos.Size = new System.Drawing.Size(776, 330);
+            this.gridAlumnos.TabIndex = 0;
+            this.gridAlumnos.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridAlumnos_CellDoubleClick);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(8, 37);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(260, 20);
+            this.txtBuscar.TabIndex = 1;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(104, 25);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 0;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(104, 63);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(366, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtApeP
+            // 
+            this.txtApeP.Location = new System.Drawing.Point(104, 108);
+            this.txtApeP.Name = "txtApeP";
+            this.txtApeP.Size = new System.Drawing.Size(366, 20);
+            this.txtApeP.TabIndex = 2;
+            // 
+            // txtAPM
+            // 
+            this.txtAPM.Location = new System.Drawing.Point(104, 154);
+            this.txtAPM.Name = "txtAPM";
+            this.txtAPM.Size = new System.Drawing.Size(366, 20);
+            this.txtAPM.TabIndex = 3;
+            // 
+            // numEdad
+            // 
+            this.numEdad.Location = new System.Drawing.Point(104, 203);
+            this.numEdad.Name = "numEdad";
+            this.numEdad.Size = new System.Drawing.Size(120, 20);
+            this.numEdad.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nombres";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Apellido paterno";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Apellido materno";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 210);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Edad";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(104, 275);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 10;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(203, 275);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 11;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(304, 275);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 12;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(406, 275);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 13;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridAlumnos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numEdad)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView gridAlumnos;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numEdad;
+        private System.Windows.Forms.TextBox txtAPM;
+        private System.Windows.Forms.TextBox txtApeP;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtID;
+    }
+}
+
---
+++ PEscuela/PEscuela/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PEscuela
+{
+    public partial class Form1 : Form
+    {
+        private bool IsNuevo = false;
+        private bool IsEditar = false;
+        EscuelaEntities bd;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Habilitar(false);
+            botones();
+            Limpiar();
+            ListarAlumnos();
+        }
+
+        private void Habilitar(bool valor)
+        {
+          
+                txtNombre.ReadOnly = !valor;
+                txtApeP.ReadOnly = !valor;
+                txtAPM.ReadOnly = !valor;
+                numEdad.ReadOnly = !valor;
+           
+           
+        }
+
+        private void Limpiar()
+        {
+            txtID.Clear();
+            txtNombre.Clear();
+            txtApeP.Clear();
+            txtAPM.Clear();
+            numEdad.Value = 0;
+        }
+
+        private void botones()
+        {
+            if (IsNuevo|| IsEditar)
+            {
+                btnNuevo.Enabled = false;
+                btnEditar.Enabled = false;
+                btnGuardar.Enabled = true;
+                btnCancelar.Enabled = true;
+            }
+            else
+            {
+                btnNuevo.Enabled = true;
+                btnEditar.Enabled = false;
+                btnGuardar.Enabled = false;
+                btnCancelar.Enabled = false;
+            }
+           
+        }
+
+        private void ListarAlumnos()
+        {
+            using(bd = new EscuelaEntities())
+            {
+                List<T_Alumnos> Lalumnos = new List<T_Alumnos>();
+                Lalumnos = bd.T_Alumnos.ToList();
+                gridAlumnos.DataSource = null;
+                gridAlumnos.DataSource = Lalumnos;
+            }
+        }
+
+        private void BuscarAlumno(string TexoBuscar)
+       {
+            using (bd = new EscuelaEntities())
+            {
+                List<T_Alumnos> Lalumnos = new List<T_Alumnos>();
+                //Lalumnos = bd.T_Alumnos.Where(x => x.Nombre.Contains(TexoBuscar) || x.ApellidoPaterno.Contains(TexoBuscar) || x.ApellidoMaterno.Contains(TexoBuscar)).ToList();
+                //gridAlumnos.DataSource = null;
+                //gridAlumnos.DataSource = Lalumnos;
+                var Result = from A in bd.T_Alumnos
+                             where A.Nombre.Contains(TexoBuscar)
+                             select A;
+                Lalumnos = Result.ToList();
+                gridAlumnos.DataSource = null;
+                gridAlumnos.DataSource = Lalumnos;
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+
+            IsNuevo = true;
+            IsEditar = false;
+            Habilitar(true);
+            botones();
+            Limpiar();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+          
+            IsNuevo = false;
+            IsEditar = true;
+            Habilitar(true);
+            botones();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            IsNuevo = false;
+            IsEditar = false;
+            botones();
+            Habilitar(false);
+            Limpiar();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IsNuevo)
+                {
+                    if (txtNombre.Text == "")
+                    {
+                        MessageBox.Show("El nombre es requerido");
+
+                    }
+                    else
+                    {
+                        using(bd= new EscuelaEntities())
+                        {
+                            T_Alumnos Alumno = new T_Alumnos();
+                            Alumno.Nombre = txtNombre.Text.ToUpper().Trim();
+                            Alumno.ApellidoPaterno = txtApeP.Text.ToUpper().Trim();
+                            Alumno.ApellidoMaterno = txtAPM.Text.ToUpper().Trim();
+                            Alumno.edad =(int)numEdad.Value;
+
+                            bd.T_Alumnos.Add(Alumno);
+                            int rpta = 0;
+                            rpta = bd.SaveChanges();
+                            if (rpta != 0)
+                            {
+                                MessageBox.Show("Alumno registrado");
+                                Limpiar();
+                                IsNuevo = false;
+                                IsEditar = false;
+                                botones();
+                                Habilitar(false);
+                                ListarAlumnos();
+                            }
+                        }
+                    }
+
+                }
+                else
+                {
+                    using (bd = new EscuelaEntities())
+                    {
+                        T_Alumnos Alumno = new T_Alumnos();
+                        int id = 0;
+                        id = Convert.ToInt32(txtID.Text);
+                        Alumno = bd.T_Alumnos.Where(x => x.idEstudiante == id).FirstOrDefault();
+                        Alumno.Nombre = txtNombre.Text.ToUpper().Trim();
+                        Alumno.ApellidoPaterno = txtApeP.Text.ToUpper().Trim();
+                        Alumno.ApellidoMaterno = txtAPM.Text.ToUpper().Trim();
+                        Alumno.edad = (int)numEdad.Value;
+
+                       
+                        int rpta = 0;
+                        rpta = bd.SaveChanges();
+                        if (rpta != 0)
+                        {
+                            MessageBox.Show("Alumno editado");
+                            Limpiar();
+                            IsNuevo = false;
+                            IsEditar = false;
+                            botones();
+                            Habilitar(false);
+                            ListarAlumnos();
+                        }
+                    }
+                }
+
+            }catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void gridAlumnos_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int id = 0;
+            id = Convert.ToInt32(gridAlumnos.CurrentRow.Cells[0].Value);
+            using (bd = new EscuelaEntities())
+            {
+                T_Alumnos alumno = new T_Alumnos();
+                alumno = bd.T_Alumnos.Where(x => x.idEstudiante == id).FirstOrDefault();
+                txtID.Text = alumno.idEstudiante.ToString();
+                txtNombre.Text = alumno.Nombre;
+                txtApeP.Text = alumno.ApellidoPaterno;
+                txtAPM.Text = alumno.ApellidoMaterno;
+                numEdad.Value = Convert.ToDecimal(alumno.edad);
+            }
+            btnEditar.Enabled = true;
+
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            BuscarAlumno(txtBuscar.Text);
+        }
+    }
+}
